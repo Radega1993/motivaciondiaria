086b2ba45a56b619c0263644d9d8b57c1b5203e5
raul <rauldearriba@gmail.com>
2022-01-03 15:11:26 +0100

    diseño 1

--- motivaciondiaria/lib/main.dart
+++ motivaciondiaria/lib/main.dart
@@ -7,25 +7,19 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Paint paint = Paint();
-    paint.color = Colors.brown.shade200;
-    Paint paint2 = Paint();
-    paint2.color = Colors.purple.shade200;
-    Paint paint3 = Paint();
-    paint3.color = Colors.blueGrey.shade200;
-    const primaryColor = Color(0xFF795548);
+    const primaryColor = Color(0xFF000000);
 
     return MaterialApp(
-        title: 'Motivación diaria',
+        title: 'MOTIVACIÓN DIARIA',
         theme: ThemeData(
           primaryColor: primaryColor,
         ),
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          backgroundColor: Colors.grey[850],
+          backgroundColor: Colors.grey.shade900,
           appBar: AppBar(
               title: Center(
-            child: Text('Motivación diaria'),
+            child: Text('MOTIVACIÓN DIARIA'),
           )),
           body: Center(
             child: Column(
@@ -37,35 +31,33 @@ class MyApp extends StatelessWidget {
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 25,
-                    background: paint3,
                     color: Colors.white,
                   ),
                 ),
-                SizedBox(height: 100),
+                SizedBox(height: 40),
                 Text(
-                  'El único modo de hacer un gran trabajo es amar lo que haces ',
+                  'El único modo de hacer un gran trabajo es amar lo que haces.',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 25,
-                    background: paint,
                     color: Colors.white,
                   ),
                 ),
                 SizedBox(height: 10),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Container(
-                    margin: new EdgeInsets.symmetric(horizontal: 30.0),
-                    child: Text(
-                      '- Steve Jobs ',
-                      textAlign: TextAlign.right,
-                      style: TextStyle(
-                        fontSize: 20,
-                        background: paint2,
-                        color: Colors.white,
-                      ),
-                    ),
+                Text(
+                  '- Steve Jobs -',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
                   ),
+                ),
+                SizedBox(height: 10),
+                Image.asset(
+                  'assets/logo.png',
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.cover,
                 ),
               ],
             ),
